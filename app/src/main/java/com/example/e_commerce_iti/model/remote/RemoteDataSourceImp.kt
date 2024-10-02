3fa9b53fb6a4487d2f2b91ae6ff5b9a49be3cc0e
ahmed hassan <ahmedhassan@fci.helwan.edu.eg>
2024-10-02 22:50:09 +0300

    create the IReposiatory interface with one function to get The Brands also create the reposiatory implementation too

--- app/src/main/java/com/example/e_commerce_iti/model/remote/RemoteDataSourceImp.kt
+++ app/src/main/java/com/example/e_commerce_iti/model/remote/RemoteDataSourceImp.kt
@@ -4,7 +4,6 @@ import com.example.e_commerce_iti.model.apis.RetrofitHelper
 import com.example.e_commerce_iti.model.pojos.BrandData
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOf
 
 class RemoteDataSourceImp : IRemoteDataSource {
     override suspend fun getBrands(): Flow<List<BrandData>> = flow{
@@ -18,7 +17,6 @@ class RemoteDataSourceImp : IRemoteDataSource {
                 imageWidth = it.image?.width,
                 imageHeight = it.image?.height
             )
-
 
         }
         emit(brands)
