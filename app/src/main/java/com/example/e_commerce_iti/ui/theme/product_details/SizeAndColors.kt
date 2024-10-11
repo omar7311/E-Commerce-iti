a2b241b5d8153c6d2765f92fc701bf0535188a7c
Omar Gohary <omar.gohary123@gmail.com>
2024-10-11 04:55:03 +0200

    adding sizes and color

--- app/src/main/java/com/example/e_commerce_iti/ui/theme/product_details/SizeAndColors.kt
+++ app/src/main/java/com/example/e_commerce_iti/ui/theme/product_details/SizeAndColors.kt
@@ -1,20 +1,35 @@
 package com.example.e_commerce_iti.ui.theme.product_details
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.e_commerce_iti.navyBlue
+import com.example.e_commerce_iti.pastelBrush
+import com.example.e_commerce_iti.transparentBrush
+import com.example.e_commerce_iti.ui.theme.home.CustomText
 
 @Composable
 fun SizeAndColors(colors:List<String>,sizes:List<String>){
@@ -24,17 +39,49 @@ fun SizeAndColors(colors:List<String>,sizes:List<String>){
         elevation = CardDefaults.cardElevation(defaultElevation = 10.dp)){
         Row(modifier = Modifier.fillMaxWidth().wrapContentHeight(),
             horizontalArrangement = Arrangement.spacedBy(12.dp)) {
-            Text(text = "Sizes:", fontSize = 18.sp)
-            LazyRow {  }
+            Text(text = "Sizes:", fontSize = 18.sp, modifier = Modifier.padding(8.dp))
+            LazyRow(
+                modifier = Modifier.wrapContentWidth()
+            ) {
+                items(sizes) { item ->
+                    // Customize each item in the LazyRow
+                    CustomText(item, pastelBrush)
+                    Spacer(Modifier.width(8.dp))
+                }
+            }
         }
         Row(modifier = Modifier.fillMaxWidth().wrapContentHeight(),
             horizontalArrangement = Arrangement.spacedBy(12.dp) ) {
-            Text(text = "Colors", fontSize = 18.sp)
+            Text(text = "Colors:", fontSize = 18.sp, modifier = Modifier.padding(8.dp))
+            LazyRow(
+                modifier = Modifier.wrapContentWidth().padding(top = 8.dp)
+            ) {
+                items(colors) { item ->
+                    // Customize each item in the LazyRow
+                    Box(modifier = Modifier.size(25.dp).background(color = colors(item) , shape = CircleShape)
+                        .border(1.dp, Color.Black , CircleShape))
+                    Spacer(Modifier.width(8.dp))
+                }
+            }
         }
     }
 }
 
+fun colors(color:String):Color{
+    return if(color=="red") Color.Red
+    else if(color=="yellow") Color.Yellow
+    else if(color=="white") Color.White
+    else if(color=="black") Color.Black
+    else if(color=="blue") Color.Blue
+    else if(color.contains("gray")) Color.DarkGray
+    else Color.LightGray
+}
+
+
+@Preview(showBackground = true)
 @Composable
 fun SizeAndColorsPreview(){
     var colors= listOf("red","yellow","white","black","blue","orange")
+    var sizes= listOf("1","2","3","4","5","6")
+SizeAndColors(colors, sizes)
 }
