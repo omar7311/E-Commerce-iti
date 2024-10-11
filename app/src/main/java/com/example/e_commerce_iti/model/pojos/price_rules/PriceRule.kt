766b1ad277165410bfdc9d8ccaa4437576280af6
mostafagamal-cell <76878167+mostafagamal-cell@users.noreply.github.com>
2024-10-11 16:10:07 +0300

    add

--- app/src/main/java/com/example/e_commerce_iti/model/pojos/price_rules/PriceRule.kt
+++ app/src/main/java/com/example/e_commerce_iti/model/pojos/price_rules/PriceRule.kt
@@ -2,7 +2,7 @@ package com.example.e_commerce_iti.model.pojos.price_rules
 
 data class PriceRule(
     val admin_graphql_api_id: String,
-    val allocation_limit: Any,
+    val allocation_limit: Any?=null,
     val allocation_method: String,
     val created_at: String,
     val customer_segment_prerequisite_ids: List<Any>,
@@ -17,8 +17,8 @@ data class PriceRule(
     val prerequisite_collection_ids: List<Any>,
     val prerequisite_customer_ids: List<Any>,
     val prerequisite_product_ids: List<Any>,
-    val prerequisite_quantity_range: Any,
-    val prerequisite_shipping_price_range: Any,
+    val prerequisite_quantity_range: Any?,
+    val prerequisite_shipping_price_range: Any?,
     val prerequisite_subtotal_range: PrerequisiteSubtotalRange,
     val prerequisite_to_entitlement_purchase: PrerequisiteToEntitlementPurchase,
     val prerequisite_to_entitlement_quantity_ratio: PrerequisiteToEntitlementQuantityRatio,
---
+++ app/src/test/java/com/example/e_commerce_iti/FakeRepo.kt
@@ -0,0 +1,4 @@
+package com.example.e_commerce_iti
+
+class FakeRepoe {
+}
