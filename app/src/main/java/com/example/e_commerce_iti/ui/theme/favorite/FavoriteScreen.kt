9649a29126ea90b0a4806ae9de699e2da34ad469
ahmed hassan <ahmedhassan@fci.helwan.edu.eg>
2024-10-13 16:54:40 +0300

    fix showing favorite button in guest mode

--- app/src/main/java/com/example/e_commerce_iti/ui/theme/favorite/FavoriteScreen.kt
+++ app/src/main/java/com/example/e_commerce_iti/ui/theme/favorite/FavoriteScreen.kt
@@ -97,9 +97,6 @@ fun FavoriteScreen(
             if (Firebase.auth.currentUser != null && !Firebase.auth.currentUser!!.email.isNullOrBlank()) {
 
                 if (currentUser.observeAsState().value!=null){
-
-
-
                 LaunchedEffect(Unit) {
                     currentUser.value!!.fav.let { cartViewModel.getCartDraftOrder(it) }
                 }
